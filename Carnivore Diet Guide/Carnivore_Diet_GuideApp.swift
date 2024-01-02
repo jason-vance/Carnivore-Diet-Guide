d7b5125edb08cde924def58896306581be5e0161
Jason Vance <jasonvance77@gmail.com>
2024-01-01 23:19:29 -1000

    Initial Commit

---
+++ Carnivore Diet Guide/Carnivore_Diet_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carnivore_Diet_GuideApp.swift
+//  Carnivore Diet Guide
+//
+//  Created by Jason Vance on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Carnivore_Diet_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
