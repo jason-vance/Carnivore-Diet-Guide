3c26262819f5ec499c2cfe4496e34b2720e3b29b
Jason Vance <jasonvance77@gmail.com>
2024-02-22 19:07:50 -1000

    Add CGFloat and Font extensions

---
+++ Carnivore Diet Guide/Extensions/CGFloatExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  CGFloatExtensions.swift
+//  Carnivore Diet Guide
+//
+//  Created by Jason Vance on 2/22/24.
+//
+
+import Foundation
+
+extension CGFloat {
+    
+    static let titleTextSize: CGFloat = 32
+    static let headerTextSize: CGFloat = 16
+    static let bodyTextSize: CGFloat = 14
+
+    static let defaultCornerRadius: CGFloat = 16
+}
---
+++ Carnivore Diet Guide/Extensions/FontExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  FontExtensions.swift
+//  Carnivore Diet Guide
+//
+//  Created by Jason Vance on 2/22/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static let regularTitle: Font = .system(size: .titleTextSize)
+    static let boldTitle: Font = .system(size: .titleTextSize, weight: .bold)
+    static let boldHeader: Font = .system(size: .headerTextSize, weight: .bold)
+    static let bodyText: Font = .system(size: .bodyTextSize)
+}
