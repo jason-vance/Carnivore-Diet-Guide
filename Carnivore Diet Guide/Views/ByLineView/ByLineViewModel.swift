3829d63815970a048d5a8bd3bf530ac0824ff3aa
Jason Vance <jasonvance77@gmail.com>
2025-10-10 23:50:23 -1000

    Fix Posts ByLineView updating with UserData late

--- Carnivore Diet Guide/Views/ByLineView/ByLineViewModel.swift
+++ Carnivore Diet Guide/Views/ByLineView/ByLineViewModel.swift
@@ -32,15 +32,17 @@ class ByLineViewModel: ObservableObject {
         initializationState = .uninitialized
         
         sub = userFetcher.fetchUser(userId: userId)
-            .receive(on: RunLoop.main)
-            .sink { [weak self] userData in
-                self?.authorProfilePicUrl = userData.profileImageUrl
-                if let userFullName = userData.fullName?.value {
-                    self?.authorDisplayName = String(localized: .init(userFullName))
-                } else if let username = userData.username?.value {
-                    self?.authorDisplayName = username
-                }
-                self?.initializationState = .initialized
-            }
+            .sink(receiveValue: onUpdate(userData:))
+    }
+    
+    @MainActor
+    private func onUpdate(userData: UserData) {
+        self.authorProfilePicUrl = userData.profileImageUrl
+        if let userFullName = userData.fullName?.value {
+            self.authorDisplayName = String(localized: .init(userFullName))
+        } else if let username = userData.username?.value {
+            self.authorDisplayName = username
+        }
+        self.initializationState = .initialized
     }
 }
